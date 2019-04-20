3f3dd3eebfb0e06671b32d9b0a8c5242687d13d8
Charles Reitz <cgreitz@hotmail.com>
2019-04-20 11:48:49 -0700

    Remove some templating, start transactions view

---
+++ charlie-web-demo/client/transactions/transactions.js
@@ -0,0 +1,13 @@
+import { Template } from 'meteor/templating';
+
+Template.transactions.onCreated(function onCreated() {
+
+});
+
+Template.transactions.helpers({
+
+});
+
+Template.transactions.events({
+
+});
